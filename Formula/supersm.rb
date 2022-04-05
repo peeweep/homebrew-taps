0398574fab360d78857ef2418bebca79e68f8f32
peeweep <peeweep@0x0.ee>
2022-04-05 02:37:47 +0000

    supersm: update homepage

--- Formula/supersm.rb
+++ Formula/supersm.rb
@@ -1,20 +1,10 @@
 class Supersm < Formula
   desc "Super Symlink Manager"
-  homepage "https://github.com/peeweep/supersm"
-  url "https://github.com/peeweep/supersm/archive/0.5.2.tar.gz"
-  sha256 "299699209c2af0f91edd659578116dbaac0aa0cd3bd0ad55be6571350fd448da"
+  homepage "https://codeberg.org/peeweep/supersm"
+  url "https://codeberg.org/peeweep/supersm/archive/0.5.2.tar.gz"
+  sha256 "85b051c80f6e1e0b43664e3a7b849c91922c808223b6d9427a0e027f9e5bb48d"
   license "MIT"
-  head "https://github.com/peeweep/supersm.git", branch: "master"
-
-  livecheck do
-    url :homepage
-  end
-
-  bottle do
-    root_url "https://ghcr.io/v2/peeweep/taps"
-    rebuild 2
-    sha256 cellar: :any, big_sur: "741fb7f0fa5e736f81ac1813e676d444bcd694efc2f1c54646eb002746d07697"
-  end
+  revision 1
 
   depends_on "cmake" => :build
   depends_on "boost"
